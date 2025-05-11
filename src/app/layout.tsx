69a39ecf8aa046e6f86601fcf70b13b34acfdbb7
DeepLore894 <deeplore894@gmail.com>
2025-05-12 06:41:34 +0700

    second

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Source_Sans_3 } from 'next/font/google';
 
+const sourceSans3 = Source_Sans_3({
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-source-sans-3',
+});
 
 export const metadata: Metadata = {
   title: 'Сейсмоизоляция',
